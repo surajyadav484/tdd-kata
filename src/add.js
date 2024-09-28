9af6c14e8952917abccd466ee8610e76bb747079
surajyadav484 <surajryadav20@gmail.com>
2024-09-28 14:31:32 +0530

    add: completed add function to fulfill test cases

--- src/add.js
+++ src/add.js
@@ -2,4 +2,30 @@ export default function add(args) {
   if (!args) {
     return 0;
   }
+  args = args.trim();
+
+  let customSeparator = null;
+  let numbers = [];
+
+  if (args.startsWith("//")) {
+    const newLineIndex = args.indexOf("\n");
+    customSeparator = args.substring(2, newLineIndex);
+    numbers = args.slice(newLineIndex + 1).split(customSeparator);
+  } else {
+    numbers = args.replace(/\n/g, ",").split(",");
+  }
+
+  const negativeNumbers = [];
+  const sum = numbers.reduce((acc, cur) => {
+    if (cur < 0) {
+      negativeNumbers.push(cur);
+    } else {
+      return acc + parseInt(cur);
+    }
+  }, 0);
+
+  if (negativeNumbers.length) {
+    return `Negative numbers not allowed ${negativeNumbers.join(",")}`;
+  }
+  return sum;
 }
